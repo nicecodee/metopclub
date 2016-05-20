d828a05526a041e62f16faf5ae52474a86a05abc
Ubuntu <ubuntu@ip-172-31-31-230.ap-northeast-1.compute.internal>
2016-05-20 06:57:46 +0000

    these files work perfectly

---
+++ content_mgmt.py
@@ -0,0 +1,26 @@
+##// CURRENT:
+##// list of topics by {TOPIC:[["TITLE", "URL"]]}
+##
+##// FUTURE:
+##////list of topics by {TOPIC:[["TITLE", "URL", "TAGS"],
+##////                          ["TITLE", "URL", "TAGS"]]}
+
+
+
+
+def Content():
+##    //             Suggest Branches for next steps
+##    //             If liked: Matplotlib, link to data analysis or Pandas maybe
+##    //             If liked: GUI stuff: Kivy, PyGame, Tkinter
+##    //             if liked: Text and word-based: NLTK
+
+
+    # MAIN : [TITLE, URL, BODY_TEXT (LIST), HINTS(LIST)]
+    TOPIC_DICT = {"Basics":[["Python Introduction","/introduction-to-python/"],
+                            ["Sounds and Music","/adding-sounds-music-pygame/"],
+							["Math bacis","/math-bacis-to-python/"]]}
+
+
+    return TOPIC_DICT
+
+
